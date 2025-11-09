5866ca010026a460cb909a5bb2c5ded7e279d17f
victor-desktop <victor.hedegran@gmail.com>
2025-11-09 11:43:39 +0100

    solution day 1 part 1

--- 2015/day_1/part_1/src/main.rs
+++ 2015/day_1/part_1/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("src/input.txt").expect("file not parsed");
+
+    fn convert_input_to_integer(c: char) -> i32 {
+        if c == '(' {
+            return 1
+        } else if c == ')' {
+            return -1
+        } else {
+            return 0
+        }
+    }
+
+    let char_vector: Vec<char> = contents.chars().collect();
+
+    let int_vector: Vec<i32> = char_vector.iter().map(|c: &char| convert_input_to_integer(*c)).collect();
+
+    let sum: i32 = int_vector.iter().fold(0, |acc: i32, &x| acc + x);
+    
+    println!("{:?}", sum);
 }
