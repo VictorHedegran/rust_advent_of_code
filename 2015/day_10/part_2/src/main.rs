94d8d157cb5f4f5c7dab43f7fd671f90162ed4af
victor-desktop <victor.hedegran@gmail.com>
2025-11-15 13:35:55 +0100

    solution for 2015 day 10

---
+++ 2015/day_10/part_2/src/main.rs
@@ -0,0 +1,41 @@
+use ::std::fs;
+
+const MAX_ITERATION: usize = 50;
+
+fn main() {
+    let content = fs::read_to_string("src/input.txt").unwrap();
+
+    fn look_and_say(current_string: String, current_iteration: usize) -> usize {
+        println!("DEBUG {}, {}", current_string, current_iteration);
+        let mut previous_chars: Option<(char, usize)> = None;
+
+        let mut next_string: String = String::new();
+
+        current_string.chars().for_each(|c| match previous_chars {
+            Some(prev) => {
+                if prev.0 == c {
+                    previous_chars = Some((prev.0, prev.1 + 1))
+                } else {
+                    next_string += &format!("{}{}", prev.1, prev.0);
+                    previous_chars = Some((c, 1))
+                }
+            }
+            None => previous_chars = Some((c, 1)),
+        });
+
+        match previous_chars {
+            Some(prev) => {
+                next_string += &format!("{}{}", prev.1, prev.0);
+            }
+            None => {}
+        }
+
+        if current_iteration < MAX_ITERATION {
+            look_and_say(next_string, current_iteration + 1)
+        } else {
+            next_string.len()
+        }
+    }
+
+    println!("{}", look_and_say(content, 1));
+}
